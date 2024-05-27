2792478579f01bb9164ea477ba339b61e95c58ef
shamshaev <an.shamshaev@gmail.com>
2024-05-27 22:46:31 +0300

    Change HelloWorld

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -10,8 +10,13 @@ public class HelloWorld {
         });
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.result("This is Javalin app in action"));
-        app.get("/users", ctx -> ctx.result("GET /users\n"));
-        app.post("/users", ctx -> ctx.result("POST /users\n"));
+        app.get("/users", ctx -> ctx.result("GET /users"));
+        app.post("/users", ctx -> ctx.result("POST /users"));
+        app.get("/users/{id}/posts/{postId}", ctx -> {
+            var id = ctx.pathParamAsClass("id", Integer.class).get();
+            var postId = ctx.pathParamAsClass("postId", Integer.class).get();
+            ctx.result("User ID: " + id + ", post ID: " + postId);
+        });
         app.get("/hello", ctx -> {
             var name = ctx.queryParam("name") != null ? ctx.queryParam("name") : "World";
             ctx.result("Hello, " + name + "!");
