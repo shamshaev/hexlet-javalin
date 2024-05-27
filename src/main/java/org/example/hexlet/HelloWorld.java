8cf7835b66c56059a3f8588df161ff7b34d5069a
shamshaev <an.shamshaev@gmail.com>
2024-05-27 20:05:53 +0300

    Change HelloWorld, .gitignore and add Makefile

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -9,7 +9,13 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
         });
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> ctx.result("This is Javalin app in action"));
+        app.get("/users", ctx -> ctx.result("GET /users\n"));
+        app.post("/users", ctx -> ctx.result("POST /users\n"));
+        app.get("/hello", ctx -> {
+            var name = ctx.queryParam("name") != null ? ctx.queryParam("name") : "World";
+            ctx.result("Hello, " + name + "!");
+        });
         app.start(7070); // Стартуем веб-сервер
     }
 }
