21a12b641bd78a32b1a34f29dbaada040a705342
shamshaev <an.shamshaev@gmail.com>
2024-06-07 18:42:40 +0300

    Change schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id bigint PRIMARY KEY AUTO_INCREMENT,
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255) NOT NULL,
     email varchar(50) NOT NULL,
     password varchar(50) NOT NULL
